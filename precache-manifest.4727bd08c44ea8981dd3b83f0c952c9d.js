a338c05fd08ef58ea9e8f900b998e67960764b69
Ilya <targix8@gmail.com>
2019-09-25 17:27:40 +0600

    Updates

---
+++ precache-manifest.4727bd08c44ea8981dd3b83f0c952c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afecf98101d9b4ee4cc814311a7a4a21",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c983ec8bd774132e61fc",
+    "url": "/monsters-rolodex/static/css/main.04dbec0e.chunk.css"
+  },
+  {
+    "revision": "783bb2d55f15e42d1998",
+    "url": "/monsters-rolodex/static/js/2.f3e65479.chunk.js"
+  },
+  {
+    "revision": "c983ec8bd774132e61fc",
+    "url": "/monsters-rolodex/static/js/main.808d74bf.chunk.js"
+  },
+  {
+    "revision": "dec695a0a0afe960dd22",
+    "url": "/monsters-rolodex/static/js/runtime-main.666675a2.js"
+  }
+]);
